3eef78312f7b3dbb87eadd35b336631ec9abf972
kkukelka <17953978+kkukelka@users.noreply.github.com>
2026-03-01 13:22:41 +0100

    feat: add quicklink for trailer

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,7 +1,32 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const isVideoPlayerOpen = ref(false);
+const youtubeId = "J8XGf_Ik_0M";
+
+const openVideoPlayer = () => {
+  isVideoPlayerOpen.value = true;
+};
+</script>
+
 <template>
   <div class="relative">
     <SettingsMenu />
+    <UButton
+      color="neutral"
+      variant="outline"
+      size="xl"
+      class="absolute top-5 right-17 z-90"
+      @click="openVideoPlayer"
+    >
+      {{ $t("movie.trailer") }}
+    </UButton>
     <NavigationMap />
+    <VideoPlayerModal
+      v-model:open="isVideoPlayerOpen"
+      :youtube-id="youtubeId"
+      title="Trailer"
+    />
     <slot />
     <div
       class="hidden lg:flex absolute right-5 bottom-5 items-center gap-1 z-90"
